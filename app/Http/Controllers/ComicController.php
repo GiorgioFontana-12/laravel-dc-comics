598de49d0781dd5878c78efbadad503254dafcad
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-07-05 17:17:10 +0200

    feat edit product to fix2

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -83,7 +83,7 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comic $comic)
     {
         $data = $request->all();
 
