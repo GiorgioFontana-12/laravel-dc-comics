4ca9ea6c22664ac6f19c1325732c2178c2609b59
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-07-06 18:26:50 +0200

    feat visual error on createPage

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -20,36 +20,59 @@
                 @csrf
             
                 <label for="name">title</label>
-                <input class="form-control"  @error('title') is-invalid @enderror" type="text" name="title" value="{{ old("title") }}">
+                <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" value="{{ old("title") }}">
                 @error("title")
-                <div class="invalid-feedback">{{$message}}</div>
+                <div class="invalid-feedback">Il titolo deve essere di almeno cinque lettere e massimo di 50 e non puo esseere lasciato vuoto</div>
                 @enderror
 
 
                 <label for="name">description</label>
-                <textarea name="description" class="form-control" cols="30" rows="5"></textarea>
+                <textarea name="description" class="form-control  @error('description') is-invalid @enderror" cols="30" rows="5">{{ old("description") }}</textarea>
+                @error("description")
+                <div class="invalid-feedback">{{$message}}</div>
+                @enderror
 
                 <label for="name">thumb</label>
-                <input class="form-control" type="text" name="thumb">
+                <input class="form-control @error('thumb') is-invalid @enderror" type="text" name="thumb" value="{{old("thumb")}}">
+                @error("thumb")
+                <div class="invalid-feedback">{{$message}}</div>
+                @enderror
 
                 <label for="name">price</label>
-                <input class="form-control" type="number" name="price">
+                <input class="form-control @error('price') is-invalid @enderror" type="number" name="price" value="{{old("price")}}">
+                @error("price")
+                    <div class="invalid-feedback">{{$message}}</div>
+                @enderror
 
                 <label for="name">series</label>
-                <input class="form-control" type="text" name="series">
+                <input class="form-control @error('series') is-invalid @enderror" type="text" name="series" value="{{old("series")}}">
+                @error("series")
+                <div class="invalid-feedback">{{$message}}</div>
+                @enderror
 
                 <label for="name">sale_date</label>
-                <input class="form-control" type="date" name="sale_date">
+                <input class="form-control @error('sale_date') is-invalid @enderror" type="date" name="sale_date" value="{{old("sale_date")}}">
+                @error("sale_date")
+                    <div class="invalid-feedback">{{$message}}</div>
+                @enderror
 
                 <label for="name">type</label>
-                <input class="form-control" type="text" name="type">
+                <input class="form-control @error('type') is-invalid @enderror" type="text" name="type" value="{{old("type")}}">
+                @error("type")
+                <div class="invalid-feedback">{{$message}}</div>
+                @enderror
 
                 <label for="name">artists</label>
-                <input class="form-control" type="text" name="artists">
+                <input class="form-control @error('artists') is-invalid @enderror" type="text" name="artists" value="{{old("artists")}}">
+                @error("artists")
+                <div class="invalid-feedback">{{$message}}</div>
+                @enderror
 
                 <label for="name">writers</label>
-                <input class="form-control" type="text" name="writers">
-
+                <input class="form-control @error('writers') is-invalid @enderror" type="text" name="writers" value="{{old("writers")}}">
+                @error("writers")
+                <div class="invalid-feedback">{{$message}}</div>
+                @enderror
                 <input class="form-control mt-4 btn btn-primary" type="submit" value="Invia">
              </form>
         </div>
